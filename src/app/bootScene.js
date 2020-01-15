1cefa17c669258759c638c90dde074e582355a5d
samme <samme@users.noreply.github.com>
2020-01-15 11:03:36 -0800

    Change scenes in update()

--- src/app/bootScene.js
+++ src/app/bootScene.js
@@ -43,7 +43,9 @@ export default {
       frameRate: 10,
       repeat: -1
     });
+  },
 
+  update: function () {
     this.scene.start('menu');
     // this.scene.start('play');
     // this.scene.remove();
