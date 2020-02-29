74e6a42f102af2b87264bea45a9ec83b557d85fc
samme <samme@users.noreply.github.com>
2020-02-29 10:41:13 -0800

    New banner

--- src/app/gameConfig.js
+++ src/app/gameConfig.js
@@ -9,7 +9,7 @@ export default {
   width: 800,
   height: 600,
   pixelArt: true,
-  title: 'Phaser 3 with Parcel 📦',
+  title: 'Phaser 3 with Parcel 🚢 (imports)',
   url: 'https://github.com/samme/phaser-parcel',
   banner: { text: 'white', background: ['#FD7400', '#FFE11A', '#BEDB39', '#1F8A70', '#004358'] },
   scene: [bootScene, menuScene, playScene, endScene],
