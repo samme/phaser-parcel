4c2f4a6ce6df1e584b3c48b6d4bc505c45cf1fd2
samme <samme@users.noreply.github.com>
2020-02-29 10:42:11 -0800

    Reorganize imports

--- src/app/plugins.js
+++ src/app/plugins.js
@@ -1,19 +1,37 @@
+// Core plugins. Required
 import 'phaser/src/cameras/2d/CameraManager';
 import 'phaser/src/events/EventEmitter';
-// import 'phaser/src/gameobjects';
-import 'phaser/src/gameobjects/image/ImageFactory';
 import 'phaser/src/gameobjects/DisplayList';
-// import 'phaser/src/gameobjects/GameObjectCreator';
+import 'phaser/src/gameobjects/UpdateList';
+import 'phaser/src/scene/ScenePlugin';
+
+// Game Objects
+
+// import 'phaser/src/gameobjects';
+
 import 'phaser/src/gameobjects/GameObjectFactory';
-import 'phaser/src/gameobjects/shape/rectangle/Rectangle';
+
+// Factories include the class
+import 'phaser/src/gameobjects/image/ImageFactory';
 import 'phaser/src/gameobjects/shape/rectangle/RectangleFactory';
 import 'phaser/src/gameobjects/text/static/TextFactory';
-import 'phaser/src/gameobjects/UpdateList';
+
+// Input
+
 import 'phaser/src/input/InputPlugin';
 import 'phaser/src/input/keyboard/KeyboardPlugin';
+
+// Loader
+
 // import 'phaser/src/loader';
+
+import 'phaser/src/loader/LoaderPlugin';
+
+// File types
 import 'phaser/src/loader/filetypes/ImageFile';
 import 'phaser/src/loader/filetypes/SpriteSheetFile';
-import 'phaser/src/loader/LoaderPlugin';
+
+// Scene plugins
+// See Phaser.Plugins.DefaultPlugins.DefaultScene
+
 import 'phaser/src/physics/arcade/ArcadePhysics';
-import 'phaser/src/scene/ScenePlugin';
