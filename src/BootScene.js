1abbfde29a4e603956e94afe06b60e8eebabf357
samme <samme@users.noreply.github.com>
2021-07-15 19:26:51 -0700

    Use const

--- src/BootScene.js
+++ src/BootScene.js
@@ -7,8 +7,8 @@ export default class BootScene extends Phaser.Scene {
   }
 
   preload () {
-    var bg = this.add.rectangle(400, 300, 400, 30, 0x666666);
-    var bar = this.add.rectangle(bg.x, bg.y, bg.width, bg.height, 0xffffff).setScale(0, 1);
+    const bg = this.add.rectangle(400, 300, 400, 30, 0x666666);
+    const bar = this.add.rectangle(bg.x, bg.y, bg.width, bg.height, 0xffffff).setScale(0, 1);
 
     console.table(images);
 
--- src/PlayScene.js
+++ src/PlayScene.js
@@ -16,14 +16,14 @@ export default class PlayScene extends Phaser.Scene {
   create () {
     this.add.image(400, 300, 'space');
 
-    var emitter = this.add.particles('red')
+    const emitter = this.add.particles('red')
       .createEmitter({
         speed: 100,
         scale: { start: 1, end: 0 },
         blendMode: 'ADD'
       });
 
-    var logo = this.physics.add.image(400, 100, 'logo')
+    const logo = this.physics.add.image(400, 100, 'logo')
       .setVelocity(100, 200)
       .setBounce(1, 1)
       .setCollideWorldBounds(true);
